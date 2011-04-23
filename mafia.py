3454da51f6b2e8b4b6528a63e1a3050114257451
Jack Edge <yellowbounder@gmail.com>
2011-04-23 19:01:34 +0100

    The calander is dumb

--- mafia.py
+++ mafia.py
@@ -83,35 +83,6 @@ class Game(object):
                 return target
         return False
 
-
-class Calander(object):
-    def __init__(self,nightzero=False):
-        self._timer = 2 #time // 2 for daynumber, % 2 for daynight
-        self._beginning = ["Game Start"]
-        if nightzero:
-            self._beginning.append("Night 0")
-    def __iter__(self):
-        return self
-    def next(self):
-        # If there's stuff in the beginning queue
-        if self._beginning:
-            item = self._beginning.pop(0)
-            return item
-
-        # Else
-        oldtimer = self._timer
-        day = self._timer // 2
-        daynight = self._timer % 2 # 0day, 1night
-
-        if daynight == 0:
-            item = "Day {0}".format(day)
-        elif daynight == 1:
-            item = "Night {0}".format(day)
-
-        # Before we return stuff, increment the timer
-        self._timer += 1
-        return item
-
 class MafiaException(Exception):
     pass
 
